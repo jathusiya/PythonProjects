318a4995b62844844faf579a683d40812711c50b
jathusiya <52482829+jathusiya@users.noreply.github.com>
2023-11-14 14:23:45 +0530

    Update AudioFromVedio.py

--- AudioFromVedio.py
+++ AudioFromVedio.py
@@ -1,6 +1,9 @@
-import moviepy.editor
+# import moviepy.editor
 
-video = moviepy.editor.VideoFileClip("xyz.mp4")
+# video = moviepy.editor.VideoFileClip("xyz.mp4")
+from moviepy.editor import VideoFileClip
+
+video = VideoFileClip("xyz.mp4")
 
 audio = video.audio
 audio.write_audiofile("extrasong.mp3")
